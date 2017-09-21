e0fd76e8f539b8e8bb549235e903887208001e24
hongbaotong <410536770@qq.com>
2017-09-21 18:26:04 +0800

    拆分数据表

--- database/migrations/2017_09_13_025809_create_users_table.php
+++ database/migrations/2017_09_13_025809_create_users_table.php
@@ -31,18 +31,6 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
 	    DB::statement("ALTER TABLE `{$usersTable}` comment '用户表'");
-
-	    ///
-	    $userCodesTable = 'user_codes';
-	    Schema::create($userCodesTable, function (Blueprint $table) {
-	        $table->increments('id');
-	        $table->unsignedInteger('create_uid')->comment('创建者ID');
-	        $table->unsignedInteger('use_uid')->default(0)->comment('使用者ID，0表示未使用');
-	        $table->char('code', 10)->comment('邀请码');
-	        $table->timestamp('create_at')->comment('创建时间');
-	        $table->timestamp('use_at')->nullable()->comment('使用时间');
-        });
-	    DB::statement("ALTER TABLE `{$userCodesTable}` comment '用户邀请码表'");
     }
 
     /**
